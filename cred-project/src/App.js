9f0f861c188362b290bc784b7191b3df4bb4cf2a
AmanKumarWeb19 <akgupta0524@gmail.com>
2023-12-23 15:06:14 +0530

    add project

---
+++ cred-project/src/App.js
@@ -0,0 +1,17 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import "../src/App.css";
+
+function App() {
+  return (
+    <div
+      style={{ backgroundColor: "#242424" }}
+      className="row mx-0 px-3 py-2 position-relative h-100"
+    >
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ cred-project/src/components/YourMoney.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const YourMoney = () => {
+  return (
+    <div className="moneyContainer">
+      <div className="moneyTitle">
+        <h2>YOUR MONEY</h2>
+      </div>
+      <div>
+        <p>CRED cash</p>
+        <p>₹60,000</p>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default YourMoney;
