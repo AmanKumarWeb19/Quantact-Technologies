50dc12713fdd2bca0e7b0433fa5a4b823f01e628
AmanKumarWeb19 <akgupta0524@gmail.com>
2023-12-24 15:58:37 +0530

    login page

--- cred-project/src/components/Login.jsx
+++ cred-project/src/components/Login.jsx
@@ -9,6 +9,22 @@ const Login = () => {
         <p className="text">membership application</p>
         <h4 className="sectext">tell us your mobile number</h4>
       </div>
+      {/* 2div */}
+      <div className="mobileinput-container">
+        <div className="input-container">
+          <input className="input-tel" type="tel" />
+        </div>
+        <div className="checkbox">
+          <input className="inputcheckbox" type="checkbox" />
+          <span style={{ backgroundColor: "white",fontSize:"12px" }}>
+            allow CRED to access your information from{" "}
+            <span style={{ fontWeight: "bold", backgroundColor: "white" }}>
+              RBI approved bureaus
+            </span>
+            this doses not impact your credit score
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
